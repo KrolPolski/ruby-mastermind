6b4daf0d088075c7705589a7a648b3bcb4f66078
Ryan Boudwin <rboudwin@student.hive.fi>
2025-05-18 16:11:09 +0300

    Now able to print instructions, and use strings to print guess and result boxes as needed

--- board.rb
+++ board.rb
@@ -1,16 +1,32 @@
-class Board
-  RED = "\x1b[2;37;41m  \033[0m"
-  GREEN = "\x1b[2;37;42m  \033[0m"
-  BLUE = "\x1b[2;37;44m  \033[0m"
-  YELLOW = "\x1b[2;37;43m  \033[0m"
-  MAGENTA = "\x1b[2;37;45m  \033[0m"
-  CYAN = "\x1b[2;37;46m  \033[0m"
+require 'colorize'
 
+class Board
+#   RED = "\x1b[2;37;41m  \033[0m"
+#   GREEN = "\x1b[2;37;42m  \033[0m"
+#   BLUE = "\x1b[2;37;44m  \033[0m"
+#   YELLOW = "\x1b[2;37;43m  \033[0m"
+#   MAGENTA = "\x1b[2;37;45m  \033[0m"
+#   CYAN = "\x1b[2;37;46m  \033[0m"
+  
   def initialize(secret_code)
     @secret_code = secret_code
+	@attempts = 0
+	@box = "  "
+	@guess_hash = {'R': @box.on_red, 'B': @box.on_blue, 'G': @box.on_green, 'Y': @box.on_light_yellow}
+	@result_hash = {'R': @box.on_red, 'W': @box.on_white}
   end
-  def print_board
-    puts "\033[H"
-    puts "BOARD HERE"
+  def print_guess(guess)
+	#p guess
+	#p @guess_hash
+	#puts @guess_hash[:'R']
+	@attempts += 1
+	print "Guess #{@attempts}: "
+	guess.each_char {|char| print @guess_hash[char.to_sym], ' '}
+	#print "Guess #{@attempts}: #{@guess_hash[guess[0].to_sym]} #{@guess_hash[guess[1].to_sym]} #{@guess_hash[guess[2].to_sym]} #{@guess_hash[guess[3].to_sym]} "
+  end
+  def print_result(result)
+    print "Result #{@attempts}: "
+	result.each_char {|char| print @result_hash[char.to_sym], ' '}
+	print "\n"
   end
 end
--- mastermind.rb
+++ mastermind.rb
@@ -5,10 +5,44 @@ class Mastermind
   #   return [RED, GRN, BLU, YEL]
   #end
   def initialize
-    @board = Board.new(4321)
-    @board.print_board  
+    @board = Board.new("RBBY")
+    new_game
   end
-
+  def instructions
+    puts "INSTRUCTIONS: "
+	puts "\nYour objective is to guess a secret sequence of four colored pegs, in 12 guesses or less. Each square represents a peg."
+	puts "\nRules:"
+	puts "1) The secret sequence is made up of only Red, Green, Blue or Yellow."
+	puts "2) a color may be repeated in the sequence as many times as desired."
+	puts "\n You will be prompted to enter a guess as a string of exactly four characters, like so:"
+	puts "Guess: BBBB"
+	@board.print_guess("BBBB")
+	@board.print_result("RR")
+	puts "\nThe result gives you feedback on your guess as follows:"
+	puts "\n1) Each red result peg means you have one peg of the correct color in the correct position."
+	puts "2) Each white result peg means you have one peg of a correct color in an incorrect position."
+	puts "3) The result positions do not correlate to specific guess positions, they merely tell you how many you have correct or nearly correct."
+	puts "\nSo in our example, we know that two of the pegs are blue, but we don't know which ones are right."
+	puts "Each guess gives you more feedback and enables you to make better guesses."
+	puts "Guess: BBGG"
+	@board.print_guess("BBGG")
+	@board.print_result("RW")
+	puts "\nSo now you would know that one of the blue pegs is in the first two positions, and one of them is elsewhere."
+	puts "And you could conclude that there are no green pegs since the number of result pegs did not change."
+	puts "\nIf you identify the correct pattern in 12 guesses or less, you win!"
+  end
+  def new_game
+	puts "Welcome to Mastermind."
+	puts "Do you want to read the instructions before you play? [Y/N]"
+	input = gets.chomp
+	if input.upcase == 'Y'
+		instructions
+	elsif input.upcase == 'N'
+		puts "Okay let's just play"
+	else
+		puts "WTF dude"
+	end
+  end
 end
 
 game = Mastermind.new
